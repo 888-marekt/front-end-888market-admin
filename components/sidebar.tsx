38d968a818e28dcdb02046dce670970288894e42
YonasWorkneh <yonasdegefu158@gmail.com>
2025-05-14 15:41:19 +0300

    push

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -86,7 +86,7 @@ export function Sidebar() {
         <Link
           href={"/flashsales"}
           className={`flex w-full items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-100 ${
-            activeTab === "flash-sales"
+            activeTab === "flashsales"
               ? "bg-blue-50 text-blue-600 border-l border-blue-600"
               : "text-gray-500"
           }`}
@@ -110,7 +110,7 @@ export function Sidebar() {
         <Link
           href={"/orderlist"}
           className={`flex w-full items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-100 ${
-            activeTab === "order-list"
+            activeTab === "orderlist"
               ? "bg-blue-50 text-blue-600 border-l border-blue-600"
               : "text-gray-500"
           }`}
