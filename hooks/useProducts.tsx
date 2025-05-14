616193b4f0dc15670e471c797444ac1128f6305e
YonasWorkneh <yonasdegefu158@gmail.com>
2025-05-14 23:14:51 +0300

    useProudct

---
+++ hooks/useProducts.tsx
@@ -0,0 +1,30 @@
+import { loginUser } from "@/lib/api/auth";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export function useProducts() {
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: login,
+    isPending: isLogggingIn,
+    isSuccess,
+    isError,
+    error,
+  } = useMutation({
+    mutationFn: loginUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["login"] });
+    },
+    onError: (err: any) => {
+      console.error("Login Error:", err?.message || "Unknown Error");
+    },
+  });
+
+  return {
+    login,
+    isLogggingIn,
+    isSuccess,
+    isError,
+    error,
+  };
+}
