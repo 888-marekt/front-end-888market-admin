7628ae72a8ae7054dc63e870808fa6e938db52d8
YonasWorkneh <yonasdegefu158@gmail.com>
2025-05-16 01:22:31 +0300

    side bar left

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -140,7 +140,7 @@ export function Sidebar() {
                     <div className="absolute left-0 top-1/2 w-4 h-0.5 bg-blue-400" />
                     <Link
                       href="/category"
-                      className={`flex w-full items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-100 ${
+                      className={`ml-3 flex w-full items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-100 ${
                         activeTab === "dashboard"
                           ? "bg-blue-50 text-blue-600 border-l border-blue-600"
                           : "text-gray-500"
@@ -161,7 +161,7 @@ export function Sidebar() {
                     <div className="absolute left-0 top-1/2 w-4 h-0.5 bg-gray-300" />
                     <Link
                       href="/subcategory"
-                      className={`flex w-full items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-100 ${
+                      className={`ml-3 flex w-full items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-100 ${
                         activeTab === "dashboard"
                           ? "bg-blue-50 text-blue-600 border-l border-blue-600"
                           : "text-gray-500"
